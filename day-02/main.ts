3db92a91770a9679c78c9e7ec261a76d8df4310b
Johan M. von Behren <johan@vonbehren.eu>
2021-12-05 20:10:03 +0100

    Add solution for part 2 of day 2

--- day-02/main.ts
+++ day-02/main.ts
@@ -68,3 +68,31 @@ const position = new Position();
 position.applyCommands(commands);
 
 console.log('part 1: ' + position.x * position.depth);
+
+// part 2
+class Position2 extends Position {
+    public aim: number;
+    constructor() {
+        super();
+        this.aim = 0;
+    }
+    public move(command: Command): void {
+        switch (command.direction) {
+            case Direction.Up:
+                this.aim -= command.steps;
+                break;
+            case Direction.Down:
+                this.aim += command.steps;
+                break;
+            case Direction.Forward:
+                this.x += command.steps;
+                this.depth += command.steps * this.aim;
+                break;
+        }
+    }
+}
+
+const position2 = new Position2();
+position2.applyCommands(commands);
+
+console.log('part 2: ' + position2.x * position2.depth);
